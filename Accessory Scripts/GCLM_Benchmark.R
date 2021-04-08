675525f3e62819fdccde95759a97b258c3fbb009
Jeremiah J. Nieves <jjniev01@users.noreply.github.com>
2021-04-08 12:19:21 +0100

    Update GCLM_Benchmark.R
    Fixed the window sizes which must be odd numbers and added a section on printing the progress

--- Accessory Scripts/GCLM_Benchmark.R
+++ Accessory Scripts/GCLM_Benchmark.R
@@ -2,11 +2,17 @@ require(raster)
 require(glcm)
 require(data.table)
 require(microbenchmark)
-
+##  Row number to start our benchmarking on in case we need to restart because 
+##  of a crash or an interrupt:
+inter_start <- NULL
+##  Number of repetitions per benchmark:
 repetitions <- 10
+##  Raster dimensions in numbers of pixels:
 ras_dims <- c(100,500,seq(1000,5000,by=1000),10000,50000,100000)
+## Numebr of grey levels to use in quantizization:
 grey_levels <- c(8,16,48,124,256)
-search_window <- c(3,5,10,20)
+##  Size of rectangular search window in pixels (must be odd to be able to have a "center":
+search_window <- c(3,5,11,21)
 
 ##  Get the original unique lengths of each parameterL
 n_ras_dims <- length(ras_dims)
@@ -36,6 +42,7 @@ process_stats_dt<-process_stats_dt[!duplicated(process_stats_dt),]
 ##  List to hold rasters:
 test_ras_list<-vector(mode="list",length=n_ras_dims)
 
+## Create the test rasters:
 for(i in 1:length(test_ras_list)){
   set.seed(245634)
   ##  Create a raster with values between 0 and 1 whose probability density 
@@ -47,7 +54,11 @@ for(i in 1:length(test_ras_list)){
   rm(tmp_ras)
 }
 
-for(i in 1:nrow(process_stats_dt)){
+
+
+##  Run the benchmarks:
+for(i in 1:ifelse(!is.null(inter_start),interstart,nrow(process_stats_dt))){
+  print(paste0("Processing combination: ", i, " of ",nrow(process_stats_dt),"..."))
   ## Run the microbenchmark:
   bench_results <- summary(microbenchmark(glcm(test_ras_list[[process_stats_dt[i,RAS.INDEX]]],
          ##  Retrieve our grey levels we are using to quantize things
